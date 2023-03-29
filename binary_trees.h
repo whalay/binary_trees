225381af0487c35c0f3c93068508b007fd840c6a
Yusuf <soliuy5@gmail.com>
2023-03-29 04:58:31 -0700

    fuction that tranverse in postorder

--- binary_trees.h
+++ binary_trees.h
@@ -24,6 +24,9 @@ struct binary_tree_s
 };
 typedef struct binary_tree_s binary_tree_t;
 
+/* Printing helper function */
+void binary_tree_print(const binary_tree_t *);
+
 /* Task function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
